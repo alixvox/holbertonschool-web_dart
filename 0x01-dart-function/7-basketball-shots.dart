6df2c853155230f4c00b0799c10b21fd5a641962
alixvox <alixvox@gmail.com>
2023-10-18 23:36:26 -0500

    Fixing task 7

--- 0x01-dart-function/7-basketball-shots.dart
+++ 0x01-dart-function/7-basketball-shots.dart
@@ -1,8 +1,8 @@
 // 7-basketball-shots.dart
 
 int whoWins(Map<String, int> teamA, Map<String, int> teamB) {
-  int totalA = teamA['Free throws']! + (teamA['2 pointer']! * 2) + (teamA['3 pointer']! * 3);
-  int totalB = teamB['Free throws']! + (teamB['2 pointer']! * 2) + (teamB['3 pointer']! * 3);
+  int totalA = (teamA['Free throws'] ?? 0) + ((teamA['2 pointer'] ?? 0) * 2) + ((teamA['3 pointer'] ?? 0) * 3);
+  int totalB = (teamB['Free throws'] ?? 0) + ((teamB['2 pointer'] ?? 0) * 2) + ((teamB['3 pointer'] ?? 0) * 3);
 
   return totalA > totalB ? 1 : totalA < totalB ? 2 : 0;
 }
