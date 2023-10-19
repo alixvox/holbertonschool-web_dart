580eb470a82585c4c4587f8590243d307f680e65
alixvox <alixvox@gmail.com>
2023-10-18 23:44:03 -0500

    adding up to task 1 on Classes

---
+++ 0x02-dart-classes/0-class.dart
@@ -0,0 +1,9 @@
+// 0-class.dart
+
+class User {
+  String? name;
+
+  String showName() {
+    return "Hello $name";
+  }
+}
---
+++ 0x02-dart-classes/1-usertojson.dart
@@ -0,0 +1,17 @@
+// 1-usertojson.dart
+
+class User {
+  String name;
+  int age;
+  double height;
+
+  User({required this.name, required this.age, required this.height});
+
+  Map<String, dynamic> toJson() {
+    return {
+      'name': name,
+      'age': age,
+      'height': height,
+    };
+  }
+}
