8209f9fd804eefc425ef9a4cb622706692e07343
alixvox <alixvox@gmail.com>
2023-10-29 20:48:44 -0500

    Adding task 0 for async

--- 0x03-asynchronous/0-users_count.dart
+++ 0x03-asynchronous/0-users_count.dart
@@ -1,8 +1,8 @@
 // 0-users_count.dart
 
-import '0-util.dart';  // Import the utility file to access the fetchUsersCount() function.
+import '0-util.dart';
 
 Future<void> usersCount() async {
-  int count = await fetchUsersCount();  // Wait for the fetchUsersCount() function to complete.
+  int count = await fetchUsersCount();
   print(count);
 }
